3759652887fa53b0b371b0e8349d22db8cf5c43f
ahmedali <imahmedalibhatti@gmailcom>
2022-03-16 16:21:19 +0500

    feat: add helper for ensuring block

--- src/helpers/block.ts
+++ src/helpers/block.ts
@@ -1,7 +1,8 @@
-import { Block } from "@polkadot/types/interfaces/runtime";
+import { Block as RuntimeBlock } from "@polkadot/types/interfaces/runtime";
+import { Block } from "../types";
 
-export const getBlockTimestamp = (block: Block): Date => {
-  const extrinsicForSetTimestamp = block.extrinsics.find((item) => {
+export const getBlockTimestamp = (block: RuntimeBlock): Date => {
+  const extrinsicForSetTimestamp = block.extrinsics.find(item => {
     return item.method.method === "set" && item.method.section === "timestamp";
   });
 
@@ -11,3 +12,11 @@ export const getBlockTimestamp = (block: Block): Date => {
 
   return new Date();
 };
+
+export async function ensureBlock(id: string): Promise<void> {
+  const block = await Block.get(id);
+
+  if (!block) {
+    await new Block(id).save();
+  }
+}
