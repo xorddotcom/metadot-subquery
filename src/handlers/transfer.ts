bb96684f2664d412b86a9d837545d8b6f572f823
ahmedali <imahmedalibhatti@gmailcom>
2022-03-29 17:18:21 +0500

    refactor: add polkadot token name and decimals

--- src/handlers/transfer.ts
+++ src/handlers/transfer.ts
@@ -1,35 +1,38 @@
 import { Balance } from "@polkadot/types/interfaces";
 import { SubstrateEvent } from "@subql/types";
 
-import { SupportedTokens, TOKEN_INFO } from "../helpers/token";
+import { getPolkadotDecimalsType, getTokenInfo, SupportedTokens } from "../constants/token";
+import { calculateFees } from "../helpers/fees";
 import { Transfer } from "../types";
 import { ensureAccounts, updateTransferStatistics } from "./account";
 import { ensureBlock } from "./block";
 import { ensureToken } from "./token";
 
 export async function transferHandler(event: SubstrateEvent): Promise<void> {
-  const name = TOKEN_INFO[SupportedTokens.KUSAMA].name;
-  const decimals = TOKEN_INFO[SupportedTokens.KUSAMA].decimals;
-
   const data = event.event.data;
   const from = data[0].toString();
   const to = data[1].toString();
   const amount = data[2];
 
-  const blockId = event.block.block.hash.toString();
   const blockNumber = event.block.block.header.number.toNumber();
-  const expendedDecimals = BigInt("1" + "0".repeat(decimals));
+  const { name, decimals } = getTokenInfo(
+    SupportedTokens.POLKADOT,
+    getPolkadotDecimalsType(blockNumber)
+  );
+  const blockId = event.block.block.hash.toString();
+  const modifiedDecimals = BigInt("1" + "0".repeat(decimals));
   const transformedAmount = (amount as Balance).toBigInt();
   const extrinsicHash = event.extrinsic?.extrinsic.hash.toString();
   const timestamp = event.block.timestamp;
-  const isSuccess = event.extrinsic ? event.extrinsic.success : true;
+  const isSuccess = event.extrinsic ? event.extrinsic.success : false;
+  const fees = event.extrinsic ? calculateFees(event.extrinsic) : BigInt(0);
 
   // to and from
   await ensureAccounts([to, from]);
   await updateTransferStatistics([to, from]);
 
   // set token details
-  await ensureToken(name, expendedDecimals);
+  await ensureToken(name, modifiedDecimals);
 
   // ensure block
   await ensureBlock(blockId);
@@ -42,6 +45,7 @@ export async function transferHandler(event: SubstrateEvent): Promise<void> {
   entity.timestamp = timestamp;
   entity.extrinsicHash = extrinsicHash;
   entity.status = isSuccess;
+  entity.fees = fees;
   entity.blockId = blockId;
 
   await entity.save();
