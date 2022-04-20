e85b88da4bdcd0f30487505e0d8a8494440a1c2f
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-04-20 22:12:33 +0500

    feat: patch types and chain using command yarn patchchain <chainname>

--- chains/westend/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "",
-  genesishash: "",
-  endpoint: "",
-  dictionary: "",
-};
--- scripts/patch-chain.js
+++ scripts/patch-chain.js
@@ -2,50 +2,74 @@ const compose = require("lodash/fp/compose");
 const yaml = require("yaml");
 const fs = require("fs");
 const path = require("path");
+const { Command } = require("commander");
+const CHAIN_CONFIG = require("./chain-config.js");
 
-const CONFIG_PATH = "modified-project.yaml";
-const NETWORK = "";
+const program = new Command();
+program
+  .version("0.0.1")
+  .requiredOption(
+    "-n, --network <name>",
+    "input network name as written in chaintypes.js",
+    "polkadot"
+  )
+  .option("-b, --blockNumber <blockNumber>", "start block to index from")
+  .option("-g, --genesisHash <genesisHash>", "genesis hash of network")
+  .option("-e, --endpoint <endpoint>", "endpoint of network")
+  .option("-d, --dictionary <dictionary>", "dictionary of network");
+program.parse(process.argv);
 
-function getChainConfig() {
-  return path.resolve(__dirname, `../${NETWORK}/config.js`);
-}
+const NETWORK = program.network;
+const BASE_MANIFEST_PATH = path.resolve(__dirname, "../base-project.yaml");
+const MANIFEST_PATH = path.resolve(__dirname, "../project.yaml");
 
-function getConfigPath(configPath = CONFIG_PATH) {
-  return path.resolve(__dirname, "../", configPath);
-}
+const startBlockNumber = program.blockNumber;
 
-function readConfig(configPath = CONFIG_PATH) {
-  const content = fs.readFileSync(getConfigPath(configPath), "utf-8");
+function readManifestFile() {
+  const content = fs.readFileSync(BASE_MANIFEST_PATH, "utf-8");
   const parsedContent = yaml.parse(content);
-  console.log(parsedContent);
   return parsedContent;
 }
 
-// readConfig();
+function patchManifest(manifest) {
+  const _manifest = { ...manifest };
+  const obj = CHAIN_CONFIG[NETWORK];
 
-function patchTypesToConfig(config) {
-  const _config = { ...config };
-  console.log("patchTypesToConfig >>> ", _config);
+  if (!obj) throw Error("Network object doesnot exist in scripts/chain-config.js");
 
-  _config["network"] = {
-    genesishash: "",
-    endpoint: "",
-    dictionary: "",
+  // create chaintypes file
+  fs.writeFileSync(
+    path.resolve(__dirname, "../chaintypes.json"),
+    JSON.stringify(obj.chaintypes, null, 2),
+    {
+      encoding: "utf-8",
+    }
+  );
+
+  // update start block
+  _manifest.dataSources[0].startBlock = startBlockNumber
+    ? Number(startBlockNumber)
+    : obj.startBlock;
+
+  // update network details
+  _manifest["network"] = {
+    genesishash: obj.genesishash,
+    endpoint: obj.endpoint,
+    dictionary: obj.dictionary,
     chaintypes: {
-      file: "./chains/{network}/chaintypes.yaml",
+      file: "./chaintypes.json",
     },
   };
 
-  return _config;
+  return _manifest;
 }
 
-function writeConfig(config, configPath = CONFIG_PATH) {
-  const _config = yaml.stringify(config, undefined);
-  console.log("writeConfig >>> ", _config);
+function writeManifest(manifest) {
+  const _manifest = yaml.stringify(manifest, undefined);
 
-  // fs.writeFileSync(getConfigPath(configPath), _config, { encoding: "utf-8" });
+  fs.writeFileSync(MANIFEST_PATH, _manifest, { encoding: "utf-8" });
+  console.log(`manifest file updated with network: ${NETWORK}`);
 }
 
-const run = compose(writeConfig, patchTypesToConfig, readConfig);
-
+const run = compose(writeManifest, patchManifest, readManifestFile);
 run();
